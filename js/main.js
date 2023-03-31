37781c3f8cd47b3faad1c810916c2f69ae04e96d
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-03-31 14:26:51 +0300

    created Function updateSliderFraction

--- js/main.js
+++ js/main.js
@@ -8,3 +8,30 @@ const slider = tns({
   controls: false,
   autoplayButtonOutput: false,
 });
+
+const sliderPrev = document.querySelector("#sliderPrev");
+const sliderNext = document.querySelector("#sliderNext");
+
+sliderPrev.onclick = function () {
+  slider.goTo("prev");
+};
+sliderNext.onclick = function () {
+  slider.goTo("next");
+};
+
+// Что на старте
+const sliderInfo = slider.getInfo();
+
+const currentSliderIndexEl = document.querySelector("#sliderCurrent");
+const sliderTotalEl = document.querySelector("#sliderTotal");
+
+currentSliderIndexEl.innerText = sliderInfo.index;
+sliderTotalEl.innerText = sliderInfo.pages;
+
+// При перемещении
+
+slider.events.on("indexChanged", () => {
+  const sliderInfo = slider.getInfo();
+  currentSliderIndexEl.innerText = sliderInfo.index;
+  sliderTotalEl.innerText = sliderInfo.pages;
+});
