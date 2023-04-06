3900d0187f01a4272a2a3ea03295e58a526026ca
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-04-06 10:19:28 +0300

    added burger menu, close onClick link - 2

--- js/main.js
+++ js/main.js
@@ -39,3 +39,29 @@ updateSliderCount();
 slider.events.on("indexChanged", () => {
   updateSliderCount();
 });
+
+// burger
+const nav = document.querySelector(".header-nav__menu");
+// const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".header-nav__extra");
+const navBtnImg = document.querySelector(".nav-btn-img");
+const menuLinks = document.querySelectorAll(".header-nav__link");
+
+navBtn.onclick = () => {
+  if (nav.classList.toggle("open")) {
+    navBtnImg.src = "./img/header/nav-close.svg";
+  } else {
+    navBtnImg.src = "./img/header/nav-icon.svg";
+  }
+};
+
+(function () {
+  if (window.innerWidth <= 1140) {
+    for (let i = 0; i < menuLinks.length; i += 1) {
+      menuLinks[i].addEventListener("click", () => {
+        nav.classList.toggle("open");
+        navBtnImg.src = "./img/header/nav-icon.svg";
+      });
+    }
+  }
+})();
